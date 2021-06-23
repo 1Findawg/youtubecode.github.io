0d2b83926fc66a20294faa3c71f4ac07688b2ba2
1Findawg <81896802+1Findawg@users.noreply.github.com>
2021-06-23 00:12:38 -0600

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,1 +1,26 @@
+window.onload = function() {
+    siteTitle();
+    siteNavigation();
+    siteFooter();
+}
 
+function siteTitle() {
+    if (window.location.href.includes("home.html")) {
+        document.getElementById("header").innerHTML += "<h1 class='center'>1Findawg Youtube Projects</h1>";
+    }
+}
+
+function siteNavigation() {
+    var navigationList = ["Home"];
+    var navigationPath = ["home"];
+    var header = "<div class='navigationList'>";
+    for (var i = 0; i < navigationList.length; i++){
+        header += "<a class='listItem' href='" + navigationPath[i] + ".html'>" + navigationList[i] + "</a>";
+    }
+    header += "</div>";
+    document.getElementById("header").innerHTML += header;
+}
+
+function siteFooter() {
+    document.getElementById("footer").innerHTML = "<div class='siteFooter'>Thank you for exploring my projects site. The material is to only be used as an educational tool. Please take visit my <a href='https://www.youtube.com/user/1Findawg'>YouTube Channel</a> to get a more in depth understanding of the coding that went it this site.</div>";
+}
